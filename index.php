eedf114f054df43dfe6faa3c0216e8ec4b39c44e
Ryan Stewart <ryan@nintenfriends>
2012-06-30 23:25:19 +0100

    Fixed old code being included in the last commit.

--- index.php
+++ index.php
@@ -20,22 +20,4 @@ if($a == NULL) {
 
 echo Template::GeneratePage(array('content_output'), $PageContent);
 
-?>ere' => 'id = '.$id));
-}
-
-if($a == NULL) {
-    /* Get Page Content */
-    $PageArray = $Database->GetSingleRow('pages',array('*'),array('where' => 'id = 1'));
-}
-
-$mtime = microtime();
-$mtime = explode(" ",$mtime);
-$mtime = $mtime[1] + $mtime[0];
-$endtime = $mtime;
-$totaltime = substr(($endtime - $starttime), 0, 10);
-
-$ScriptExecution = "This page was created in ".$totaltime." seconds with a total of ".$Database->QueryCount." queries executed";
-
-echo $Template->GeneratePage(array(), array('Title' => $PageArray['title'], 'Content' => $PageArray['content'], 'ScriptExecution' => $ScriptExecution));
-
 ?>
